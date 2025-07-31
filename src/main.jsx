3691d20a1fbca0fb1e1a2a6a6edc93f87a3718aa
codechitti216 <codingchitti@gmail.com>
2025-08-01 01:35:08 +0530

    Math Rendering Test

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'katex/dist/katex.min.css'
 console.log('[DEBUG] React app loaded');
 import './index.css'
 import App from './App.jsx'
