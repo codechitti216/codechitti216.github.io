bc6d9158ed1fe5e5635c855e4259206a7ae8a8bb
codechitti216 <codingchitti@gmail.com>
2025-08-01 14:42:54 +0530

    Debugs Added 2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/codechitti216.github.io/' : '/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
